ab75735d1802cc4bdd83ab8d5a3ab9bb493a2ece
blö <hedgren.anna@gmail.com>
2019-04-12 19:22:42 +0200

    aa detta är en onepage

---
+++ javascript.js
@@ -0,0 +1,8 @@
+$("#knapp1").click(function() {
+    $('html,body').animate({
+      scrollTop:$("#block-2").offset().top}, 2000);
+    });
+    $("#knapp2").click(function() {
+        $('html,body').animate({
+          scrollTop:$("#block-1").offset().top}, 2000);
+        });
